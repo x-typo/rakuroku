f823276e0d2994ac7006b62b76eaa0c7d207afca
Ty <87083014+x-typo@users.noreply.github.com>
2025-12-24 15:49:44 -0700

    feat: change animation type of FilterModal to fade

--- src/components/FilterModal.tsx
+++ src/components/FilterModal.tsx
@@ -29,7 +29,7 @@ export function FilterModal<T extends string>({
     <Modal
       visible={visible}
       transparent
-      animationType="slide"
+      animationType="fade"
       onRequestClose={onClose}
     >
       <Pressable style={styles.overlay} onPress={onClose}>
